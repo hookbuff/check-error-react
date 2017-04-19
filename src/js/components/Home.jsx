b26499c678e33e0f0d94b97df4ccffd7e9254ba3
hookbuff <botapem@gmail.com>
2017-04-20 00:00:04 +0700

    test
    test

---
+++ src/js/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello, SiamHTML!
+            </div>
+        );
+    }
+}
+ 
+export default Home;
